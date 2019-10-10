16968a4bb54258a1147820998b398dd1d4ce0e7d
Gashira <j.hugues.guinot@gmail.com>
2019-10-10 23:20:38 +0200

    Revert "Commit before V2"
    This reverts commit 4a45a490

--- db/migrate/20190814121737_add_flag_quotation.rb
+++
@@ -1,5 +0,0 @@
-class AddFlagQuotation < ActiveRecord::Migration[5.2]
-  def change
-    add_column :wares, :show_desc_quot, :boolean
-  end
-end
--- db/migrate/20190814130050_change_comment_to_text_wares.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCommentToTextWares < ActiveRecord::Migration[5.2]
-  def change
-    change_column :wares, :comment, :text
-  end
-end
